8b8be8263872c89dda6f1ea0b23caba1e9b47acc
Shravan <shravansinghchaudhary@gmail.com>
2024-02-14 17:04:34 +0530

    feat: :sparkles: create resolver for verifying google token

--- src/user/resolvers.ts
+++ src/user/resolvers.ts
@@ -1,10 +1,47 @@
+import axios from "axios";
+import { GoogleTokenResult } from "../types";
+import { prismaClient } from "../config/db";
+import TokenService from "../services/tokenService";
+
 const queries = {
-    verifyGoogleToken: async (
-        parent: unknown,
-        { token }: { token: string },
-    ) => {
-        return token;
-    },
+  verifyGoogleToken: async (parent: unknown, { token }: { token: string }) => {
+    const googleToken = token;
+    const googleOAuthURL = new URL("https://oauth2.googleapis.com/tokeninfo");
+    googleOAuthURL.searchParams.set("id_token", googleToken);
+
+    const { data } = await axios.get<GoogleTokenResult>(
+      googleOAuthURL.toString(),
+      {
+        responseType: "json",
+      },
+    );
+
+    const user = await prismaClient.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          email: data.email,
+          firstName: data.given_name,
+          lastName: data.family_name,
+          profileImage: data.picture,
+        },
+      });
+    }
+
+    // generate JWT token
+    const userInDb = await prismaClient.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!userInDb) throw new Error("User not found");
+
+    const jwtToken = await TokenService.generateToken(userInDb);
+
+    return jwtToken;
+  },
 };
 
 export const resolvers = { queries };
